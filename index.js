1828f10679fec7a99b0e8f277070c72fb5c99e37
habib610 <habib54562@gmail.com>
2020-10-16 00:53:27 +0600

    crud remaining one

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fs = require('fs-extra');
+const fileUpload = require('express-fileupload');
 require('dotenv').config();
 
 
@@ -9,6 +11,8 @@ const port = 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static('services'));
+app.use(fileUpload());
 
 
 app.get("/", (req, res)=>{
@@ -25,6 +29,7 @@ client.connect(err => {
   const orderCollection = client.db(`${process.env.DB_NAME}`).collection("orders");
   const reviewCollection = client.db(`${process.env.DB_NAME}`).collection("reviews");
   const adminCollection = client.db(`${process.env.DB_NAME}`).collection("admins");
+  const serviceCollection = client.db(`${process.env.DB_NAME}`).collection("service");
 
 ///taking order from clients
 app.post("/takeOrder", (req, res)=>{
@@ -90,6 +95,46 @@ app.get('/getAdmin', (req, res)=>{
     })
 })
 
+app.get('/getService', (req, res)=>{
+    serviceCollection.find({})
+    .toArray((err, documents)=>{
+        res.send(documents);
+    })
+})
+
+
+
+//fileUpload files
+app.post('/addServices', (req, res)=>{
+    const file = req.files.file;
+    const title = req.body.title;
+    const description = req.body.description;
+    console.log(title, description, file);
+    const filePath = `${__dirname}/services/${file.name}`;
+    file.mv(filePath, err=>{
+        if(err){
+            console.log(err);
+            return res.status(500).send({msg: "failed to upload images"})
+        }
+        const newImg = fs.readFileSync(filePath);
+        const encImg = newImg.toString('base64');
+
+        var image ={
+            contentType: req.files.file.mimetype,
+            size: req.files.file.size,
+            img: Buffer(encImg, 'base64')
+        }
+
+        serviceCollection.insertOne({title, description, image})
+        .then(result=>{
+            fs.remove(filePath, err=>{
+                if(err){console.log(err)}
+                res.send(result.insertedCount>0)
+            })
+        })
+        // return res.send({name: file.name, path: `/${file.name}`})
+    })
+})
 
 
 
